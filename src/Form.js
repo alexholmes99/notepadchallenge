947b5410b46af77d875497a41162d32cd64a6433
Alex Holmes <alexh@chillibean.com>
2022-02-18 15:31:04 +0000

    Added date input

--- src/Form.js
+++ src/Form.js
@@ -5,6 +5,7 @@ import TextArea from "./TextArea";
 function Form(props){
   const [formData, setFormData] = useState({
     name: "",
+    date: "",
     note: ""
   })
 
@@ -20,24 +21,33 @@ function Form(props){
       alert(e.target.name + " is required")
     }
   }
-return(
-  <div>
-    <Input 
-    type="text" 
-    name="name" 
-    placeholder="Enter Your Name" 
-    onChange={handleChange} 
-    onBlur={handleBlur}
+
+  return(
+    <div>
+      <Input 
+        type="text" 
+        name="name" 
+        placeholder="Enter Your Name" 
+        onChange={handleChange} 
+        onBlur={handleBlur}
+      />
+      <Input
+        type="date"
+        name="date"
+        placeholder=""
+        value={formData.date}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+    <TextArea
+      name="note"
+      placeholder="Enter Note Here"
+      value={formData.note}
+      onChange={handleChange}
+      onBlur={handleBlur}
     />
-  <TextArea
-    name="note"
-    placeholder="Enter Note Here"
-    value={formData.note}
-    onChange={handleChange}
-    onBlur={handleBlur}
-  />
-  </div>
-)
+    </div>
+  )
 
 }
 
