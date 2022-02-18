f014cc36cb9bfbdaf8f397a1034ad04758085062
Alex Holmes <alexh@chillibean.com>
2022-02-18 14:30:44 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react"
+import './App.css';
+
+function App() {
+  const [formData, setFormData] = useState({
+    name: ""
+  })
+
+  return (
+    <div className="App">
+    </div>
+  );
+
+
+}
+
+export default App;
