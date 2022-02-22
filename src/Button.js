0f459723933cc313aaaba3e0a9170aecac8786bc
Alex Holmes <alexh@chillibean.com>
2022-02-22 15:52:36 +0000

    Added button component

---
+++ src/Button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+function Button(props) {
+  return (
+    <StyledButton type={props.type} value={props.value}>
+      {props.content}
+    </StyledButton>
+  );
+}
+
+const StyledButton = styled.button`
+  border: none;
+  opacity: 0.8;
+  color: #e33d3d;
+  font-size: 24px;
+  width: 100%;
+  font-family: "Sue Ellen Francisco", cursive;
+`;
+export default Button;
