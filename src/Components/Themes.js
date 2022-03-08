93019bbe7b2502c0a0f331a0ea36879cc95d3519
Alex Holmes <alexh@chillibean.com>
2022-03-08 15:29:37 +0000

    Added Theme component

---
+++ src/Components/Themes.js
@@ -0,0 +1,8 @@
+import { ThemeProvider } from "styled-components";
+const theme = {};
+
+const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+export default Theme;
